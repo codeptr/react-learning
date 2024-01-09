6b4811a09e1a35a4c915462ee6fad953da4bd9b4
Codeptr <i@qibu.tech>
2024-01-09 18:59:30 +0800

    Session 3

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@ import {
 } from 'react-router-dom'
 import './index.css'
 import Root from './routes/root'
+import ErrorPage from './routes/error-page'
 
 const router = new createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ])
 
