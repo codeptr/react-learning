4cba15e88765e3d729364a14b2a22cf7afb8d06f
Codeptr <i@qibu.tech>
2024-01-12 01:49:59 +0800

    Session 18

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -13,13 +13,15 @@ export async function action() {
   return redirect(`contacts/${contact.id}/edit`);
 }
 
-export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
 }
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
 
   return (
@@ -34,6 +36,7 @@ export default function Root() {
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
             />
             <div
               id="search-spinner"
