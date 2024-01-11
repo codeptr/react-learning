c0261b4fc37ce0be735479d2f1de5af205da48ff
Codeptr <i@qibu.tech>
2024-01-12 02:14:54 +0800

    Session 19

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Outlet,
   NavLink,
@@ -15,28 +16,36 @@ export async function action() {
 
 export async function loader({ request }) {
   const url = new URL(request.url);
-  const q = url.searchParams.get("q");
+  const q = url.searchParams.get("q") || "";
   const contacts = await getContacts(q);
   return { contacts, q };
 }
 
 export default function Root() {
   const { contacts, q } = useLoaderData();
+  const [query, setQuery] = useState(q);
   const navigation = useNavigation();
+
+  useEffect(() => {
+    setQuery(q);
+  }, [q]);
 
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               id="q"
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
-              defaultValue={q}
+              value={query}
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
             />
             <div
               id="search-spinner"
@@ -47,7 +56,7 @@ export default function Root() {
               className="sr-only"
               aria-live="polite"
             ></div>
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
